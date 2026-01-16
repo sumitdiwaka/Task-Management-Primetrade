cda5f2963e66b5b4e9e1a628bc41bc18c68f8218
unknown <sumitdiwakar476@gmai.com>
2026-01-16 14:34:53 +0530

    Task-Management System

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api', // Your backend URL
+    baseURL: import.meta.env.VITE_API_URL ||'http://localhost:5000/api', // Your backend URL
 });
 
 // This part automatically attaches your JWT token to every request
